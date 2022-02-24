2617e4b1fc93b8e337e227a2c45b991d7d30a471
savoloon <nugaev.vlad@mail.ru>
2022-02-24 18:18:45 +0500

    бот спрашивает кто здесь

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import telebot
+from settings import TOKEN
 
-bot = telebot.TeleBot('5183745281:AAHxc5o1_VjgqPk0_GkXOe-NJH8iEeBSij0')
+bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_massages(message):
-    bot.send_message(message.from_user.id, f"Кто здесь")
+    username = message.from_user.username
+    msg = f"Привет, {username}"
+    bot.send_message(message.from_user.id, msg)
 
+
+bot.polling()
 
 
