3f4fb8e80eb056845fdd538b162a11f911d4a4d8
savoloon <nugaev.vlad@mail.ru>
2022-03-03 03:18:28 +0500

    #2 Создание сайта про себя любимого

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import telebot
 from settings import TOKEN
-
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(content_types=['text'])
-def get_text_massages(message):
+def start(message):
     username = message.from_user.username
     msg = f"Привет, {username}"
     bot.send_message(message.from_user.id, msg)
-
 
 bot.polling()
 
